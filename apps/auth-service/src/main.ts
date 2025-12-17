3be936139867fcaf210c6a3b1e6f6175dc427e1f
Hasan Rifat <rifat@xpertfintech.com>
2025-12-17 12:57:47 +0600

    configured error handler

--- apps/auth-service/src/main.ts
+++ apps/auth-service/src/main.ts
@@ -1,14 +1,26 @@
+import { errorMiddleware } from './../../../packages/error-handler/error-middleware';
 import express from 'express';
-
-const host = process.env.HOST ?? 'localhost';
-const port = process.env.PORT ? Number(process.env.PORT) : 3000;
+import cors from 'cors';
 
 const app = express();
+
+app.use(cors({
+  origin: ['http://localhost:3000'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true,
+}));
 
 app.get('/', (req, res) => {
     res.send({ 'message': 'Hello API'});
 });
 
-app.listen(port, host, () => {
-    console.log(`[ ready ] http://${host}:${port}`);
+app.use(errorMiddleware)
+
+const port = process.env.PORT || 6001;
+const server = app.listen(port, () => {
+    console.log(`[ ready ] http://localhost:${port}`);
+});
+
+server.on('error', (err)=>{
+    console.error(err);
 });
---
+++ packages/error-handler/error-middleware.ts
@@ -0,0 +1,22 @@
+import { NextFunction, Request,Response } from "express";
+import { AppError } from ".";
+
+export const errorMiddleware = (err: any, req: Request, res: Response, next: NextFunction) => {
+  if(err instanceof AppError) {
+    console.log(`Error ${req.method} ${req.url} - ${err.message}`);
+
+    return res.status(err.statusCode).json({
+    //   success: false,
+    status: 'error',
+      message: err.message,
+      ...(err.details && { details: err.details })
+    });
+  } else {
+    console.error('Unexpected error:', err);
+    res.status(500).json({
+    //   success: false,
+      status: 'error',
+      message: 'Something went wrong, please try again later.',
+    });
+  }
+}
